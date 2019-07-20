0b6963d33a9d8d9f347447c31dc5f6422b7874fd
Ante <tolusic9@gmail.com>
2019-07-20 23:11:24 +0200

    Init commit - add Layout component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <Layout>
+      <div>
+        tole
+      </div>
+    </Layout>
+  )
+}
+
+export default App
